81b1166c02316d15b1ae7556f8b037026c48a64d
KyranMendoza1 <42067499+KyranMendoza1@users.noreply.github.com>
2024-09-08 01:15:47 +0200

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ JOBS = dict(
             benchmark_duration=10000, # amount of time to run the benchmark in milliseconds
             wait_slave_duration=5000, # amount of time to wait for slaves to post solutions before submitting
             num_jobs=1, # number of jobs to create. each job will sample its own difficulty
-            weight=1, # weight of jobs for this algorithm. more weight = more likely to be picked
+            weight=1.0, # weight of jobs for this algorithm. more weight = more likely to be picked
         )
     ),
     vehicle_routing=dict(
@@ -24,7 +24,7 @@ JOBS = dict(
             benchmark_duration=10000,
             wait_slave_duration=5000,
             num_jobs=1,
-            weight=10,
+            weight=10.0,
         )
     ),
     knapsack=dict(
@@ -32,7 +32,7 @@ JOBS = dict(
             benchmark_duration=10000,
             wait_slave_duration=5000,
             num_jobs=1,
-            weight=5,
+            weight=5.0,
         )
     ),
     vector_search=dict(
