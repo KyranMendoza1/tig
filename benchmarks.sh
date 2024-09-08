7e2b7f4ffd39d0292c59e802f8e58912c8f9e9fc
KyranMendoza1 <42067499+KyranMendoza1@users.noreply.github.com>
2024-09-08 18:14:49 +0200

    Update benchmarks.sh

--- benchmarks.sh
+++ benchmarks.sh
@@ -1,51 +1,47 @@
 #!/bin/bash
 set -e
-
 apt-get install jq -y
-
+# Ask for player_id as input
 PLAYER_ID=0x5de35f527176887b1b42a2703ba4d64e62a48de4
+BLOCK_ID=$(curl -s https://mainnet-api.tig.foundation/get-block | jq -r '.block.id')
+RESP=$(curl -s "https://mainnet-api.tig.foundation/get-benchmarks?block_id=$BLOCK_ID&player_id=$PLAYER_ID")
+
+BENCHMARKS=$(echo $RESP | jq -c '[.benchmarks[]] | sort_by(.settings.challenge_id, -.details.num_solutions)')
 
 declare -A SOLUTIONS_COUNT
-declare -A PREVIOUS_SOLUTIONS_COUNT
 
-while true; do
-    BLOCK_ID=$(curl -s https://mainnet-api.tig.foundation/get-block | jq -r '.block.id')
-    RESP=$(curl -s "https://mainnet-api.tig.foundation/get-benchmarks?block_id=$BLOCK_ID&player_id=$PLAYER_ID")
+for BENCHMARK in $(echo $BENCHMARKS | jq -c '.[]'); do
+    ID=$(echo $BENCHMARK | jq -r '.id')
+    SETTINGS=$(echo $BENCHMARK | jq -c '.settings')
+    CHALLENGE_ID=$(echo $BENCHMARK | jq -r '.settings.challenge_id')
+    NUM_SOLUTIONS=$(echo $BENCHMARK | jq -r '.details.num_solutions')
 
-    BENCHMARKS=$(echo $RESP | jq -c '[.benchmarks[]] | sort_by(.settings.challenge_id, -.details.num_solutions)')
+    # Map challenge ID to challenge name
+    case "$CHALLENGE_ID" in
+        "c001")
+            CHALLENGE_NAME="Satisfiability"
+            ;;
+        "c002")
+            CHALLENGE_NAME="Vehicle Routing"
+            ;;
+        "c003")
+            CHALLENGE_NAME="Knapsack"
+            ;;
+        "c004")
+            CHALLENGE_NAME="Vector Search"
+            ;;
+        *)
+            CHALLENGE_NAME="Unknown Challenge"
+            ;;
+    esac
 
-    for BENCHMARK in $(echo $BENCHMARKS | jq -c '.[]'); do
-        CHALLENGE_ID=$(echo $BENCHMARK | jq -r '.settings.challenge_id')
-        NUM_SOLUTIONS=$(echo $BENCHMARK | jq -r '.details.num_solutions')
-
-        # Map challenge ID to challenge name
-        case "$CHALLENGE_ID" in
-            "c001")
-                CHALLENGE_NAME="Satisfiability"
-                ;;
-            "c002")
-                CHALLENGE_NAME="Vehicle Routing"
-                ;;
-            "c003")
-                CHALLENGE_NAME="Knapsack"
-                ;;
-            "c004")
-                CHALLENGE_NAME="Vector Search"
-                ;;
-            *)
-                CHALLENGE_NAME="Unknown Challenge"
-                ;;
-        esac
-
-        # Update the solution count
-        PREVIOUS_COUNT=${SOLUTIONS_COUNT["$CHALLENGE_NAME"]:-0}
-        SOLUTIONS_COUNT["$CHALLENGE_NAME"]=$(( ${SOLUTIONS_COUNT["$CHALLENGE_NAME"]:-0} + NUM_SOLUTIONS ))
-
-        # Check if new solutions have been found
-        if [ ${SOLUTIONS_COUNT["$CHALLENGE_NAME"]} -gt $PREVIOUS_COUNT ]; then
-            echo "New solutions found for $CHALLENGE_NAME: ${SOLUTIONS_COUNT[$CHALLENGE_NAME]}"
-        fi
-    done
-
-    sleep 60  # Sleep for 1 minute before checking again
+    SOLUTIONS_COUNT["$CHALLENGE_NAME"]=$(( ${SOLUTIONS_COUNT["$CHALLENGE_NAME"]:-0} + NUM_SOLUTIONS ))
 done
+
+# Display all the challenges in one line
+OUTPUT=""
+for CHALLENGE_NAME in "Satisfiability" "Vehicle Routing" "Knapsack" "Vector Search"; do
+    OUTPUT+="$CHALLENGE_NAME: ${SOLUTIONS_COUNT[$CHALLENGE_NAME]:-0}  "
+done
+
+echo "$OUTPUT"
