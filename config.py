413c351edf16a83c9e7daaa47fabe195c6415957
KyranMendoza1 <42067499+KyranMendoza1@users.noreply.github.com>
2024-09-08 10:34:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ JOBS = dict(
         sat_optima=dict(
             benchmark_duration=10000, # amount of time to run the benchmark in milliseconds
             wait_slave_duration=5000, # amount of time to wait for slaves to post solutions before submitting
-            num_jobs=10, # number of jobs to create. each job will sample its own difficulty
+            num_jobs=15, # number of jobs to create. each job will sample its own difficulty
             weight=1.0, # weight of jobs for this algorithm. more weight = more likely to be picked
         )
     ),
