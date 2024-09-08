3e58164c21f4fa92f7a3fb18c4fed7cdcc901a81
KyranMendoza1 <42067499+KyranMendoza1@users.noreply.github.com>
2024-09-08 18:56:07 +0200

    Update benchmarks.sh

--- benchmarks.sh
+++ benchmarks.sh
@@ -1,57 +1,64 @@
 #!/bin/bash
+
+# Create a new tmux session and run the script inside it
+tmux new-session -d -s benchmark_session "bash -c '
+
 set -e
 
 # Install jq if not already installed
 apt-get install jq -y
-tmux new-session -d -s benchmark-session bash -c '
 PLAYER_ID=0x5de35f527176887b1b42a2703ba4d64e62a48de4
 
 while true; do
     # Reset SOLUTIONS_COUNT array at the start of each iteration
     declare -A SOLUTIONS_COUNT=()
 
-    BLOCK_ID=$(curl -s https://mainnet-api.tig.foundation/get-block | jq -r '.block.id')
-    RESP=$(curl -s "https://mainnet-api.tig.foundation/get-benchmarks?block_id=$BLOCK_ID&player_id=$PLAYER_ID")
+    BLOCK_ID=$(curl -s https://mainnet-api.tig.foundation/get-block | jq -r \".block.id\")
+    RESP=$(curl -s \"https://mainnet-api.tig.foundation/get-benchmarks?block_id=\$BLOCK_ID&player_id=\$PLAYER_ID\")
 
-    BENCHMARKS=$(echo $RESP | jq -c '[.benchmarks[]] | sort_by(.settings.challenge_id, -.details.num_solutions)')
+    BENCHMARKS=$(echo \$RESP | jq -c '[.benchmarks[]] | sort_by(.settings.challenge_id, -.details.num_solutions)')
 
-    for BENCHMARK in $(echo $BENCHMARKS | jq -c '.[]'); do
-        ID=$(echo $BENCHMARK | jq -r '.id')
-        SETTINGS=$(echo $BENCHMARK | jq -c '.settings')
-        CHALLENGE_ID=$(echo $BENCHMARK | jq -r '.settings.challenge_id')
-        NUM_SOLUTIONS=$(echo $BENCHMARK | jq -r '.details.num_solutions')
+    for BENCHMARK in \$(echo \$BENCHMARKS | jq -c '.[]'); do
+        ID=\$(echo \$BENCHMARK | jq -r '.id')
+        SETTINGS=\$(echo \$BENCHMARK | jq -c '.settings')
+        CHALLENGE_ID=\$(echo \$BENCHMARK | jq -r '.settings.challenge_id')
+        NUM_SOLUTIONS=\$(echo \$BENCHMARK | jq -r '.details.num_solutions')
 
         # Map challenge ID to challenge name
-        case "$CHALLENGE_ID" in
-            "c001")
-                CHALLENGE_NAME="Satisfiability"
+        case \"\$CHALLENGE_ID\" in
+            \"c001\")
+                CHALLENGE_NAME=\"Satisfiability\"
                 ;;
-            "c002")
-                CHALLENGE_NAME="Vehicle Routing"
+            \"c002\")
+                CHALLENGE_NAME=\"Vehicle Routing\"
                 ;;
-            "c003")
-                CHALLENGE_NAME="Knapsack"
+            \"c003\")
+                CHALLENGE_NAME=\"Knapsack\"
                 ;;
-            "c004")
-                CHALLENGE_NAME="Vector Search"
+            \"c004\")
+                CHALLENGE_NAME=\"Vector Search\"
                 ;;
             *)
-                CHALLENGE_NAME="Unknown Challenge"
+                CHALLENGE_NAME=\"Unknown Challenge\"
                 ;;
         esac
 
-        SOLUTIONS_COUNT["$CHALLENGE_NAME"]=$(( ${SOLUTIONS_COUNT["$CHALLENGE_NAME"]:-0} + NUM_SOLUTIONS ))
+        SOLUTIONS_COUNT[\"\$CHALLENGE_NAME\"]=\$(( \${SOLUTIONS_COUNT[\"\$CHALLENGE_NAME\"]:-0} + NUM_SOLUTIONS ))
     done
 
     # Display all the challenges in one line
-    OUTPUT=""
-    for CHALLENGE_NAME in "Satisfiability" "Vehicle Routing" "Knapsack" "Vector Search"; do
-        OUTPUT+="$CHALLENGE_NAME: ${SOLUTIONS_COUNT[$CHALLENGE_NAME]:-0}  "
+    OUTPUT=\"\"
+    for CHALLENGE_NAME in \"Satisfiability\" \"Vehicle Routing\" \"Knapsack\" \"Vector Search\"; do
+        OUTPUT+=\"\$CHALLENGE_NAME: \${SOLUTIONS_COUNT[\$CHALLENGE_NAME]:-0}  \"
     done
 
-    echo "$OUTPUT"
+    echo \"\$OUTPUT\"
 
     # Wait for 30 seconds before the next iteration
     sleep 30
 done
-'
+
+'"
+
+# Attach to the tmux session
+tmux attach-session -t benchmark_session
